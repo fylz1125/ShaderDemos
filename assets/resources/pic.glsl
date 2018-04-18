b1f39eab8065d1b0c7a2c3564beef3b55de5acf9
lerry <fylz1125@163.com>
2018-04-18 18:55:40 +0800

    修改一下shader

--- assets/resources/pic.glsl
+++ assets/resources/pic.glsl
@@ -4,8 +4,8 @@ precision mediump float;
 
 void main( void ) {
 	vec2 uv = (gl_FragCoord.xy / iResolution.xy);
-	uv.y += .02 * sin(iGlobalTime + uv.x * 2.2);
-	uv.x += .02 * sin(iGlobalTime + uv.y * 2.2);
+	uv.y += .01* sin(iGlobalTime + uv.x * 2.2);
+	uv.x += .01 * sin(iGlobalTime + uv.y * 2.2);
 	vec4 color = texture2D(iChannel0, uv);
 	color = color * color;
 	gl_FragColor = color;
