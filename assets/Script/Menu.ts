1c58d1b3aa6aef8ce371aace4a99d29288b0b9bd
lerry <fylz1125@163.com>
2018-04-28 01:28:18 +0800

    增加一个场景文件夹

--- assets/Script/Menu.ts
+++ assets/Script/Menu.ts
@@ -18,15 +18,23 @@ export default class NewClass extends cc.Component {
 
         // 初始化场景数组
         let scenes = cc.game._sceneInfos;//隐藏属性，拿过来用
+        let amazing: string[] = new Array<string>();
         for (let i = 0; i < scenes.length; ++i){
             let url = <string>scenes[i].url;
-            let tmp = url.replace('db://assets/Scene/', '').replace('.fire','');
-            if (tmp === 'GrayEffect') {
-                this.sceneList.unshift(tmp);
+            if (url.search(/AmazingEffects/) == -1) {
+                let tmp = url.replace('db://assets/Scene/', '').replace('.fire', '');
+                if (tmp === 'StartScene') {
+                    this.sceneList.unshift(tmp);
+                } else {
+                    this.sceneList.push(tmp);
+                }
             } else {
-                this.sceneList.push(tmp);
+                let tmp = url.replace('db://assets/Scene/AmazingEffects/', '').replace('.fire', '');
+                amazing.push(tmp);
             }
         }
+        this.sceneList = this.sceneList.concat(amazing);
+        
         // 说明文档必须要和场景同名
         let currentSceneName = this.sceneList[this.currentSceneIndex];
         this.loadInstruction(currentSceneName);
