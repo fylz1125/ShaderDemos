886eb212eb463240948df8831a480b93d2e39f93
lerry <fylz1125@163.com>
2018-04-29 15:56:24 +0800

    clouds的shader

--- assets/resources/glowing.glsl
+++ assets/resources/glowing.glsl
@@ -7,7 +7,7 @@ float coefficient()
 	float v = mod(iGlobalTime, 3.0);
     if(v > 1.5)
         v = 3.0 - v;
-    return 0.19;
+    return v;
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
