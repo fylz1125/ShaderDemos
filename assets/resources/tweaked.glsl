b3f96e74adb938776426e9bcf336f75577257326
lerry <fylz1125@163.com>
2018-04-29 16:54:01 +0800

    又加了一个shader

--- assets/resources/tweaked.glsl
+++ assets/resources/tweaked.glsl
@@ -4,17 +4,21 @@ precision mediump float;
 //tweaked by psyreco
 
 
-
+uniform float time;
+uniform vec2 resolution;
+varying vec2 v_texCoord;
 void main( void ) {
 
-	vec2 position = abs( gl_FragCoord.xy * 2.0 -  iResolution.xy) / min(iResolution.x, iResolution.y);
+	vec2 rs = resolution.xy;
+  	vec2 uv= v_texCoord.xy;
+	vec2 position = abs( uv*rs * 2.0 -  rs) / min(resolution.x, resolution.y);
 	vec3 destColor = vec3(.70, 0.0, .75 );
 	float f = 0.45;
 	
 	for(float i = 0.5; i < 21.0; i++){
 		
-		float s = tan(sin(iGlobalTime/3.0 - i )) ;
-		float c = atan(cos(iGlobalTime/7.0 - i ));
+		float s = tan(sin(time/3.0 - i )) ;
+		float c = atan(cos(time/7.0 - i ));
 		f +=abs(0.0047 / abs(length(5.0* position *f - vec2(c, s) ) -0.84));
 	}
 
